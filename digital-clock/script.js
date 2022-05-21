f741859af9ffa44065f5eb7e6856f05a77ef23a9
Shishir Neupane <shishirs520@gmail.com>
2022-05-21 23:45:41 +0545

    prettier formatting

--- digital-clock/script.js
+++ digital-clock/script.js
@@ -3,9 +3,9 @@ const root = document.getElementById('root');
 const currentTime = document.createElement('h1');
 
 setInterval(function () {
-	const date = new Date();
-	const time = `Current Time - ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-	currentTime.textContent = time;
+  const date = new Date();
+  const time = `Current Time - ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  currentTime.textContent = time;
 }, 1000);
 
 root.appendChild(currentTime);
@@ -27,43 +27,43 @@ const resetBtn = document.createElement('button');
 resetBtn.textContent = 'Reset';
 
 function runStopwatch() {
-	ms = ms + 5;
-	if (ms >= 1000) {
-		ms = 0;
-		s++;
-	}
-	if (s >= 60) {
-		s = 0;
-		m++;
-	}
-	if (m >= 60) {
-		m = 0;
-		h++;
-	}
-	if (h >= 24) {
-		return;
-	}
-	stopwatchTime.textContent = `Stopwatch - ${h}:${m}:${s}:${ms}`;
+  ms = ms + 4;
+  if (ms >= 1000) {
+    ms = 0;
+    s++;
+  }
+  if (s >= 60) {
+    s = 0;
+    m++;
+  }
+  if (m >= 60) {
+    m = 0;
+    h++;
+  }
+  if (h >= 24) {
+    return;
+  }
+  stopwatchTime.textContent = `Stopwatch - ${h}:${m}:${s}:${ms}`;
 }
 
 startBtn.addEventListener('click', function () {
-	startBtn.replaceWith(stopBtn);
-	interval = setInterval(runStopwatch, 1);
+  startBtn.replaceWith(stopBtn);
+  interval = setInterval(runStopwatch, 1);
 });
 
 stopBtn.addEventListener('click', function () {
-	clearInterval(interval);
-	stopBtn.replaceWith(startBtn);
+  clearInterval(interval);
+  stopBtn.replaceWith(startBtn);
 });
 
 resetBtn.addEventListener('click', function () {
-	clearInterval(interval);
-	stopBtn.replaceWith(startBtn);
-	ms = 0;
-	s = 0;
-	m = 0;
-	h = 0;
-	stopwatchTime.textContent = `Stopwatch - ${h}:${m}:${s}:${ms}`;
+  clearInterval(interval);
+  stopBtn.replaceWith(startBtn);
+  ms = 0;
+  s = 0;
+  m = 0;
+  h = 0;
+  stopwatchTime.textContent = `Stopwatch - ${h}:${m}:${s}:${ms}`;
 });
 
 root.appendChild(stopwatchTime);
